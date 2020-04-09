ceac11f0817059cce400ef3c3b049c6acce221f9
ZoceanZ <hzheng63@wisc.edu>
2020-04-09 17:18:15 -0500

    Homepage for import and part of the receipt keeping are done. Add a receipt id to the table column of the database

---
+++ app/src/main/java/com/example/better_budgets/gallery_receipt.java
@@ -0,0 +1,14 @@
+package com.example.better_budgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gallery_receipt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_receipt);
+    }
+}
