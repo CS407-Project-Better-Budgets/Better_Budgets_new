bfa6fe03d3e2616786ca2aed567390746c4fcead
ClaraZ1203 <zhang2324@wisc.edu>
2020-04-28 18:18:05 -0500

    activities outlines done

--- app/src/main/java/com/example/better_budgets/manage_data.java
+++ app/src/main/java/com/example/better_budgets/manage_data.java
@@ -2,7 +2,10 @@ package com.example.better_budgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class manage_data extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class manage_data extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_data);
+    }
+
+    public void allOnClick(View view){
+        Intent intent = new Intent(this, manage_data_all.class);
+        startActivity(intent);
+    }
+
+    public void idOnClick(View view){
+        Intent intent = new Intent(this, manage_data_id.class);
+        startActivity(intent);
+    }
+
+    public void sourceOnClick(View view){
+        Intent intent = new Intent(this, manage_data_source.class);
+        startActivity(intent);
+    }
+
+    public void dateOnClick(View view){
+        Intent intent = new Intent(this, manage_data_date.class);
+        startActivity(intent);
+    }
+
+    public void amountOnClick(View view){
+        Intent intent = new Intent(this, manage_data_amount.class);
+        startActivity(intent);
+    }
+
+    public void sellerOnClick(View view){
+        Intent intent = new Intent(this, manage_data_seller.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/better_budgets/manage_data_source.java
@@ -0,0 +1,40 @@
+package com.example.better_budgets;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class manage_data_source extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_data_source);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.manage_data_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_goto_data_management:
+                Intent intent = new Intent(this, manage_data.class);
+                startActivity(intent);
+            case R.id.item_goto_main_activity:
+                Intent intent2 = new Intent(this, MainActivity.class);
+                startActivity(intent2);
+            default: return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
